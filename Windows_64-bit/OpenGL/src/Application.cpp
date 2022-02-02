b909efbc1944d5460c506b1b86337c17d67070c3
Eric Trotchie <Eric-JT@users.noreply.github.com>
2022-02-01 21:56:26 -0800

    Added GLEW to Windows version

--- Windows_64-bit/OpenGL/src/Application.cpp
+++ Windows_64-bit/OpenGL/src/Application.cpp
@@ -1,12 +1,19 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
 
 int main( void )
 {
     GLFWwindow* window;
 
+
+
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
+
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow( 640, 480, "Hello World", NULL, NULL );
@@ -19,13 +26,18 @@ int main( void )
     /* Make the window's context current */
     glfwMakeContextCurrent( window );
 
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error! glewInit not ok." << std::endl;
+
+    std::cout << glGetString( GL_VERSION ) << std::endl;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose( window ))
     {
         /* Render here */
         glClear( GL_COLOR_BUFFER_BIT );
 
-        glBegin(GL_TRIANGLES);
+        glBegin( GL_TRIANGLES );
         glVertex2f( -0.5f, -0.5f );
         glVertex2f( -0.0f, 0.5f );
         glVertex2f( 0.5f, -0.5f );
